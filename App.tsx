2f37ed5edca9a7e9ac8c71b158b7cdf2eb013498
nujhong <w.chen-2@hotmail.com>
2022-06-17 14:48:37 +1000

    add minimal reproducible example

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import {Button, View} from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerScreenProps,
+} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+type RootStackParamList = {
+  Home: undefined;
+  Notifications: undefined;
+};
+
+function HomeScreen({
+  navigation,
+}: DrawerScreenProps<RootStackParamList, 'Home'>) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({
+  navigation,
+}: DrawerScreenProps<RootStackParamList, 'Notifications'>) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
